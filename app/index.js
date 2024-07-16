f4db2a1ad8fd1d6a19c0fa635484105562224348
galp <galp@elal.co.il>
2024-07-16 05:25:20 +0000

    index.js ready

---
+++ app/index.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const app = express()
+const port = 9001
+
+app.get('/', (req, res) => {
+  res.send('Hello World ver 0.1!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
